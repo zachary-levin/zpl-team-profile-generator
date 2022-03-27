c878e0d50dabb55ff51ec86b6422280ee14888b5
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-03-26 22:57:14 -0500

    added rest of question prompts

--- index.js
+++ index.js
@@ -23,6 +23,19 @@ const promptQuestions = () => {
         },
         {
             type: 'input',
+            name: 'id',
+            message: 'Please enter the ID of the team member. (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter an ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
             name: 'email',
             message: 'Please enter the email address of the team member. (Required)',
             validate: emailInput => {
@@ -36,12 +49,30 @@ const promptQuestions = () => {
         },
         {
             type: 'list',
-            name: 'position',
-            message: 'Please select the position for the team member. (Required)',
+            name: 'role',
+            message: 'Please select the role for the team member. (Required)',
             choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Please enter the office number of the Manager.',
+            when: ({ role }) => role.choices = Manager
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Please enter the Github username of the Engineer.',
+            when: ({ role }) => role.choices = Engineer
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Please enter the school of the Intern.',
+            when: ({ role }) => role.choices = Intern
         }
-        
-    ])
+
+    ]);
 }
 
 promptQuestions();
