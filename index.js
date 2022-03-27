1666b4b01f496f68b49266daed45b554659c2e2f
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-03-26 19:21:19 -0500

    added initial inquirer question prompts

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+
+// Create an array of questions for user input
+
+const promptQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter the name of the team member. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the email address of the team member. (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'position',
+            message: 'Please select the position for the team member. (Required)',
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+        
+    ])
+}
+
+promptQuestions();
